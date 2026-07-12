b01fe9579c89e75d5ec43050628ef5136a2db34b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "pim_eligible_role_assignments" {
-  description = "All pim_eligible_role_assignment resources"
-  value       = azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments
-}
 output "pim_eligible_role_assignments_condition" {
-  description = "List of condition values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.condition]
+  description = "Map of condition values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.condition }
 }
 output "pim_eligible_role_assignments_condition_version" {
-  description = "List of condition_version values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.condition_version]
+  description = "Map of condition_version values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.condition_version }
 }
 output "pim_eligible_role_assignments_justification" {
-  description = "List of justification values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.justification]
+  description = "Map of justification values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.justification }
 }
 output "pim_eligible_role_assignments_principal_id" {
-  description = "List of principal_id values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.principal_id]
+  description = "Map of principal_id values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.principal_id }
 }
 output "pim_eligible_role_assignments_principal_type" {
-  description = "List of principal_type values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.principal_type]
+  description = "Map of principal_type values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.principal_type }
 }
 output "pim_eligible_role_assignments_role_definition_id" {
-  description = "List of role_definition_id values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.role_definition_id]
+  description = "Map of role_definition_id values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.role_definition_id }
 }
 output "pim_eligible_role_assignments_schedule" {
-  description = "List of schedule values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.schedule]
+  description = "Map of schedule values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.schedule }
 }
 output "pim_eligible_role_assignments_scope" {
-  description = "List of scope values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.scope]
+  description = "Map of scope values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.scope }
 }
 output "pim_eligible_role_assignments_ticket" {
-  description = "List of ticket values across all pim_eligible_role_assignments"
-  value       = [for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : v.ticket]
+  description = "Map of ticket values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.ticket }
 }
 
