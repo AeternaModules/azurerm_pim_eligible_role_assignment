2091f9ae99e0c92edbf56255cce2e73adb703fcf
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,41 @@
 output "pim_eligible_role_assignments_id" {
   description = "Map of id values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.id }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "pim_eligible_role_assignments_condition" {
   description = "Map of condition values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.condition }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.condition if v.condition != null && length(v.condition) > 0 }
 }
 output "pim_eligible_role_assignments_condition_version" {
   description = "Map of condition_version values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.condition_version }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.condition_version if v.condition_version != null && length(v.condition_version) > 0 }
 }
 output "pim_eligible_role_assignments_justification" {
   description = "Map of justification values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.justification }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.justification if v.justification != null && length(v.justification) > 0 }
 }
 output "pim_eligible_role_assignments_principal_id" {
   description = "Map of principal_id values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.principal_id }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.principal_id if v.principal_id != null && length(v.principal_id) > 0 }
 }
 output "pim_eligible_role_assignments_principal_type" {
   description = "Map of principal_type values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.principal_type }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.principal_type if v.principal_type != null && length(v.principal_type) > 0 }
 }
 output "pim_eligible_role_assignments_role_definition_id" {
   description = "Map of role_definition_id values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.role_definition_id }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.role_definition_id if v.role_definition_id != null && length(v.role_definition_id) > 0 }
 }
 output "pim_eligible_role_assignments_schedule" {
   description = "Map of schedule values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.schedule }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.schedule if v.schedule != null && length(v.schedule) > 0 }
 }
 output "pim_eligible_role_assignments_scope" {
   description = "Map of scope values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.scope }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.scope if v.scope != null && length(v.scope) > 0 }
 }
 output "pim_eligible_role_assignments_ticket" {
   description = "Map of ticket values across all pim_eligible_role_assignments, keyed the same as var.pim_eligible_role_assignments"
-  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.ticket }
+  value       = { for k, v in azurerm_pim_eligible_role_assignment.pim_eligible_role_assignments : k => v.ticket if v.ticket != null && length(v.ticket) > 0 }
 }
 
